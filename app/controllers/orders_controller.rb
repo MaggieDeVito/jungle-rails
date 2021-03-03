0c3dac988677e8add567998bb5ebc7bb7528b6b6
MaggieDeVito <maggiemaydevito@gmail.com>
2021-03-03 23:14:04 +0000

    created table for products from the order to show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,11 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    # raise @order.inspect
+    @line_items = LineItem.where(order_id: @order.id)
+    @products = @line_items.map { 
+      |line_item| 
+      { product: Product.find(line_item.product_id), quantity: line_item.quantity }
+    }
   end
 
   def create
