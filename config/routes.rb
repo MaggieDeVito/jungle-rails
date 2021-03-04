ef0213d838ed1af4e6dea14036a3a87044d1a190
MaggieDeVito <maggiemaydevito@gmail.com>
2021-03-04 20:34:41 +0000

    created an about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
+  
+  get '/about', to: 'about#index'
 
   resources :orders, only: [:create, :show]
 
